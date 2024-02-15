43ac1d6c6f4700d6ac87fd0b73c67a55e655a19e
andreasmlanger <andreas.m.langer@gmail.com>
2024-02-15 12:04:11 +0100

    change youtube downloader to pytube

--- mp3_mp4_mkv/youtube_downloader.py
+++ mp3_mp4_mkv/youtube_downloader.py
@@ -1,48 +1,35 @@
 """
-Downloads a YouTube video as mp3 and webm file (optional subtitles)
-ffmpeg.exe needs to be in user directory
+Downloads a YouTube video as mp4 and mp3 file
 """
 
-from yt_dlp import YoutubeDL
+from pytube import YouTube
 import os
 
-SUBTITLES = False
-MP3 = True
-WEBM = False
-
 PATH = os.path.join(os.environ['USERPROFILE'], 'Downloads')  # downloads go here
-FFMPEG = os.path.join(os.environ['USERPROFILE'], 'Miniconda3', 'envs', 'PyCharm', 'Scripts', 'ffmpeg.exe')
 
 
-class YouTube:
+class YT:
     @staticmethod
-    def download():
-        links = str(input('\033[94m' + 'Enter video links separated by space\n')).split(' ')
-        for link in links:
-            if link != '':
-                v = {'outtmpl': PATH + r'\%(title)s.%(ext)s'}
-                if SUBTITLES:
-                    v['writeautomaticsub'] = True
-                    v['allsubtitles'] = True
-                if MP3:
-                    a = {'outtmpl': PATH + r'\%(title)s.%(ext)s',
-                         'format': 'bestaudio/best',
-                         'postprocessors': [{'key': 'FFmpegExtractAudio',
-                                             'preferredcodec': 'mp3',
-                                             'preferredquality': '192'}]}
-                    print('\033[92m' + 'Downloading audio file of ' + str(link))
-                    with YoutubeDL(a) as ydl:
-                        ydl.download([link])
-                if WEBM:
-                    print('\033[92m' + 'Downloading video file of ' + str(link))
-                    with YoutubeDL(v) as ydl:
-                        ydl.download([link])
-                print('\033[36m' + 'All files downloaded successfully!')
+    def download(yt, output_format='mp4'):
+        if output_format == 'mp4':
+            yt.streams.filter(
+                progressive=True,
+                file_extension='mp4',
+            ).order_by('resolution').desc().first().download(output_path=PATH)
+
+        elif output_format == 'mp3':
+            yt.streams.filter(
+                only_audio=True,
+            ).order_by('abr').desc().first().download(output_path=PATH)
 
 
 def main():
-    y = YouTube()
-    y.download()
+    urls = str(input('Enter YouTube video urls separated by space: ')).split(' ')
+    for url in urls:
+        yt = YouTube(url)
+        YT.download(yt, 'mp4')
+        YT.download(yt, 'mp3')
+    print('\033[36m' + 'All files downloaded successfully!')
 
 
 if __name__ == '__main__':
